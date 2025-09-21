b388e78a44b1c2a8c6d8a1ef2d687b83cc6e450b
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-09-21 00:35:03 -0300

    Implementação de testes external para o método POST do register

--- test/external/userExternal.test.js
+++ test/external/userExternal.test.js
@@ -26,5 +26,29 @@ describe('User Controller', () => {
             expect(response.status).to.equal(400);
             expect(response.body).to.have.property('message', 'Usuário e senha são obrigatórios');
         });
+
+        it('Register user successfully and return 201', async () => {
+            const response = await request(servidor)
+                .post('/users/register')
+                .send({
+                    username: "Dick",
+                    password: "asanoturna"
+                });
+
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('username', 'Bruce');
+        });
+
+        it('User exists and return 400', async () => {
+            const response = await request(servidor)
+                .post('/users/register')
+                .send({
+                    username: "Bruce",
+                    password: "batman"
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('message', 'Usuário já existe');
+        });
     });
 });
