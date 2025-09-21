5a85e6079f462d75ffc65451183acf50057347f7
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-09-21 16:39:48 -0300

    Criado testes controller (normais e sinon)  para o método POST do register do comiccs no comicController.test.js

--- models/comicModel.js
+++ models/comicModel.js
@@ -1,3 +1,11 @@
-const comics = [];
+const comics = [
+    {
+        name: "Absolute Batman 2",
+        publisher: "Panini",
+        licensor: "DC",
+        genre: "Super Hero",
+        price: 19.90
+    }
+];
 
 module.exports = comics;
