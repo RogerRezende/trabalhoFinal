c58035d083db1826642c4caa9d06b59b0108fa86
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-09-21 00:53:19 -0300

    Implementação de testes external para o método POST do register dos comics

---
+++ test/external/comicExternal.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+const servidor = 'http://localhost:3000';
+
+describe('User Controller', () => {
+    describe('POST /comics/register', () => {
+        it('Register comic successfully and return 201', async () => {
+            const responseLogin = await request(servidor)
+                .post('/users/login')
+                .send({
+                    username: "Bruce",
+                    password: "batman"
+                });
+
+            const token = responseLogin.body.token;
+
+            const response = await request(servidor)
+                .post('/comics/register')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    name: "Absolute Batman 2",
+                    publisher: "Panini",
+                    licensor: "DC",
+                    genre: "Super Hero",
+                    price: 19.90
+                });
+
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('name', 'Absolute Batman 2');
+            expect(response.body).to.have.property('publisher', 'Panini');
+            expect(response.body).to.have.property('licensor', 'DC');
+            expect(response.body).to.have.property('genre', 'Super Hero');
+            expect(response.body).to.have.property('price', 19.90);
+        });
+    });
+});
