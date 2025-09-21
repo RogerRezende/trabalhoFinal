e282a4dd7f13a2d239e24deca7ea794b117fc5da
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-09-21 17:08:10 -0300

    Criado testes external para o método GET do listar do comiccs no comicExternal.test.js

--- test/external/comicExternal.test.js
+++ test/external/comicExternal.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 
 const servidor = 'http://localhost:3000';
 
-describe('User Controller', () => {
+describe('Comic External', () => {
     describe('POST /comics/register', () => {
         it('Register comic successfully and return 201', async () => {
             const responseLogin = await request(servidor)
@@ -105,6 +105,36 @@ describe('User Controller', () => {
 
             expect(response.status).to.equal(400);
             expect(response.body).to.have.property('message', 'Todos os campos são obrigatórios');
+        });
+    });
+
+    describe('GET /comics', () => {
+        it('List comics successfully and return 200 and confirm is array', async () => {
+            const response = await request(servidor)
+                .get('/comics');
+
+            expect(response.status).to.equal(200);
+            expect(response.body).to.be.an('array');
+        });
+
+        it('List comics successfully and return 200 and confirm is not empty', async () => {
+            const response = await request(servidor)
+                .get('/comics');
+
+            expect(response.status).to.equal(200);
+            expect(response.body).to.be.an('array').that.is.not.empty;
+        });
+
+        it('List comics successfully and return 200 and confirm first comic properties', async () => {
+            const response = await request(servidor)
+                .get('/comics');
+
+            expect(response.status).to.equal(200);
+            expect(response.body[0]).to.have.property('name', 'Absolute Batman 1');
+            expect(response.body[0]).to.have.property('publisher', 'Panini');
+            expect(response.body[0]).to.have.property('licensor', 'DC');
+            expect(response.body[0]).to.have.property('genre', 'Super Hero');
+            expect(response.body[0]).to.have.property('price', 19.90);
         });
     });
 });
