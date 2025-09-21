1d62c1827816fcc1a325b0061505f0b2e1533aa8
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-09-21 15:48:46 -0300

    Criado testes de controller (normais e sinon) para o método POST do login no userController.test.js

--- test/controller/userController.test.js
+++ test/controller/userController.test.js
@@ -106,4 +106,108 @@ describe('User Controller', () => {
             sinon.restore();
         });
     });
+
+    describe('POST /users/login', () => {
+        it('Without password and return 400', async () => {
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    username: "Bruce"
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('message', 'Usuário e senha são obrigatórios');
+        });
+
+        it('Without username and return 400', async () => {
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    password: "batman"
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('message', 'Usuário e senha são obrigatórios');
+        });
+
+        it('Login user successfully and return 200', async () => {
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    username: "Bruce",
+                    password: "batman"
+                });
+
+            const token = response.body.token;
+
+            expect(response.status).to.equal(200);
+            expect(response.body).to.have.property('token', token);
+        });
+
+        it('User not exist and return 401', async () => {
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    username: "Bruce",
+                    password: "batman2"
+                });
+
+            expect(response.status).to.equal(401);
+            expect(response.body).to.have.property('message', 'Credenciais inválidas');
+        });
+
+        it('Using mocks: Without password and return 400', async () => {
+            const transferServiceMock = sinon.stub(userService, 'loginUser')
+            transferServiceMock.throws(new Error('Usuário e senha obrigatórios'));
+
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    username: "Bruce"
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('message', 'Usuário e senha são obrigatórios');
+
+            sinon.restore();
+        });
+
+        it('Using mocks: Without username and return 400', async () => {
+            const transferServiceMock = sinon.stub(userService, 'loginUser')
+            transferServiceMock.throws(new Error('Usuário e senha obrigatórios'));
+
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    password: "batman"
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('message', 'Usuário e senha são obrigatórios');
+
+            sinon.restore();
+        });
+
+        it('Using mocks: Login user successfully and return 200', async () => {
+            const transferServiceMock = sinon.stub(userService, 'loginUser')
+            transferServiceMock.returns({
+                username: "Bruce",
+                password: "batman"
+            });
+
+            const response = await request(app)
+                .post('/users/login')
+                .send({
+                    username: "Bruce",
+                    password: "batman"
+                });
+
+            const token = response.body.token;
+
+            expect(response.status).to.equal(200);
+            expect(response.body).to.have.property('token', token);
+
+            sinon.restore();
+        });
+    });
 });
